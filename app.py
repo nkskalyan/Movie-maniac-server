7de712245a2b00f4037ae50525e43fc59188e47c
Kalyan <knachiappan@groupon.com>
2015-05-10 04:36:04 +0530

    Env changes push

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
 from blueprints.api import api
+from models import db
 
 app = Flask(__name__)
+app.config.from_object('default_settings')
 app.register_blueprint(api,url_prefix="/api")
 
 @app.route('/')
@@ -10,5 +12,5 @@ def hello_world():
   return "Hello World"
 
 if __name__ == '__main__':
-  app.debug = True
+  db.init_app(app)
   app.run(host="0.0.0.0",port=8000)
